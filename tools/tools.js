d4448bbc507b99bd5be0f464012d88d988207327
Julien-Devos <72694305+Julien-Devos@users.noreply.github.com>
2021-12-03 00:56:45 +0100

    corrected date2

--- tools/tools.js
+++ tools/tools.js
@@ -2,7 +2,7 @@ module.exports = {
 
     dateToTime: (postDate) => {
         let currDate = new Date();
-        const months = ["Janv.","Févr.","Mars","Avr.","Mai","Juin","Juill.","Août","Sep.","Oct.","Nov.","Déc."];
+        const months = ["Janv.","Févr.","Mars","Avr.","Mai","Juin","Juil.","Août","Sep.","Oct.","Nov.","Déc."];
 
         const diffTime = Math.abs(currDate - postDate);
         const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
@@ -10,30 +10,23 @@ module.exports = {
         const diffMin = Math.floor(diffTime / (1000 * 60));
         const diffSec = Math.floor(diffTime / 1000);
 
-        console.log(diffHrs,diffSec);
-
         if (diffHrs < 24){
-            let postHrs = postDate.toLocaleString('fr-BE').split(",")[1].split(':');
-            let currHrs = currDate.toLocaleString('fr-BE').split(",")[1].split(':');
 
             if (diffSec < 60){
-                let secDiff = Math.round(diffSec);
-                return "Il y a " + secDiff + "s";
+                return "Il y a " + Math.round(diffSec) + "s";
             }
             else if (diffMin < 60){
-                let minDiff = Math.round(diffMin);
-                return ("Il y a " + minDiff + " min.");
+                return ("Il y a " + Math.round(diffMin) + " min.");
             }
             else {
-                let hrsDiff = Math.round(diffHrs);
-                return ("Il y a " + hrsDiff + "h");
+                return ("Il y a " + Math.round(diffHrs) + "h");
             }
         }
         else if (diffDays >= 365){
             let dateString = postDate.toLocaleString('fr-BE').split(",")[0].split('/');
             let day = dateString[0];
             let month = months[dateString[1] - 1];
-            let year = dateString[3];
+            let year = dateString[2];
 
             return (day + " " + month + " " + year);
         }
